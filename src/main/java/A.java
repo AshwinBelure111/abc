d49ab84513191fc602f3fd58861b49d8b3b4ba06
AshwiniBelure111 <ashwinibelure111@gmail.com>
2025-04-24 12:53:32 +0530

    first

---
+++ src/main/java/A.java
@@ -0,0 +1,14 @@
+
+public class A {
+    public static void main(String[] args) {
+        A a=new A();
+        a.onetoten();
+        System.out.println("Hello ");
+
+    }
+    public void onetoten() {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i);
+        }
+    }
+}
